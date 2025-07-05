7754302dafac3d14aa4f77f5b624d27c7f6cff42
Eli Grosman <eligrosman1@gmail.com>
2025-07-05 00:32:20 -0700

    fix sql syntax

--- server/src/db/schema.sql
+++ server/src/db/schema.sql
@@ -113,7 +113,7 @@ BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     RETURN NEW;
 END;
-$$ language 'plpgsql';
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
